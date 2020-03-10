3fada830c49dc43df3a18ce2caf21870810e7c7d
이진하 <ijinha@ijinhaui-MacBookPro.local>
2020-03-10 15:47:59 +0900

    기본 프로젝트 세팅

---
+++ Instargram/Instargram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instargram
+//
+//  Created by 이진하 on 2020/03/10.
+//  Copyright © 2020 이진하. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
